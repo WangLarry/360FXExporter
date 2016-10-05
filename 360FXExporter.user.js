27f8a5af510f3a314608e79eda8c0a77f7d4a3c9
WangLarry <wdg7217@163.com>
2016-10-05 17:11:02 +0800

    Update 360FXExporter.user.js

--- 360FXExporter.user.js
+++ 360FXExporter.user.js
@@ -13,6 +13,6 @@ jqscript.id = "jq_script";
 jqscript.src = "http://libs.baidu.com/jquery/1.8.3/jquery.min.js";
 var script = document.createElement('script');
 script.id = "TLE_script";
-script.src = "https://raw.github.com/chztv/360FXExporter/master/360FXExporter.js";
+script.src = "http://rawgit.com/WangLarry/360FXExporter/master/360FXExporter.js";
 document.body.appendChild(jqscript);
 document.body.appendChild(script);
